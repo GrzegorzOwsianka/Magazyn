4c13ac60b7dbb9aa1c227af3c18db6cb3a5c43d3
GrzegorzOwsianka <owsiankagrzegorz84@gmail.com>
2022-07-13 19:30:24 +0200

    ver. 1.0.3

--- src/main/java/pl/owsianka/magazyn/database/UserDatabase.java
+++ src/main/java/pl/owsianka/magazyn/database/UserDatabase.java
@@ -1,4 +1,21 @@
 package pl.owsianka.magazyn.database;
 
+import org.springframework.stereotype.Component;
+import pl.owsianka.magazyn.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class UserDatabase {
+    private List<User> users = new ArrayList<>();
+
+    public UserDatabase() {
+        users.add(new User(1,"admin","admin"));
+        users.add(new User(2,"grzegorz","grzegorz"));
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
 }
